26deff9b7c4af4a6663bb10f67ee65348f60c491
Viviano Cantu <viviano.cantu@gmail.com>
2017-05-21 22:43:18 -0400

    finish assignment 2

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var port = process.env.PORT || 52425;//3000;
+var port = process.env.PORT || 3000;
 
 app.listen(port);
